1ee762a7c0944763529a11d3ea1e3c039a4b4799
edouardsouan <souan.edouard0@gmail.com>
2015-01-21 20:40:35 +0100

    ADD Number of jobs Waiting to be treated and treated

--- worker.rb
+++ worker.rb
@@ -24,29 +24,43 @@ end
 
 class CrawlerWorker < Worker
 	def start()
+		treatedjob = 0
 		while true do
+			GetNumberOfWaitingJobs()
+			GetNumberOfTreatedJobs(treatedjob)
+
 			redis = Redis.new
 			currentJob = redis.rpop("jobsList")
-
 			if !currentJob.nil?
+				treatedjob = treatedjob + 1
 				parsedJob = JSON.parse(currentJob)
 				currentJobName = parsedJob["jobName"]
 				currentjobAction = parsedJob["action"]
 
 				puts "#{currentJobName} is running"
 
-				displayWebSiteTitle(currentjobAction)
+				DisplayWebSiteTitle(currentjobAction)
 
 			end
 			sleep 5
 		end
 	end
 
-	def displayWebSiteTitle(url)
+	def DisplayWebSiteTitle(url)
 		result = Net::HTTP.get_response(URI.parse(url)).body
 		doc = Nokogiri::HTML(result)
 		doc.xpath('//title').each do |title|
 			puts "The title of this Website is : #{title.content.split(" ")[0]}"
 		end
+	end
+
+	def GetNumberOfWaitingJobs()
+		redis = Redis.new
+		waitingJobs = redis.lrange "jobsList", 0, -1
+		puts "There is #{waitingJobs.length} jobs wainting to be treated."
+	end
+
+	def GetNumberOfTreatedJobs(treatedjob)
+		puts "#{treatedjob} job(s) treated."
 	end
 end
